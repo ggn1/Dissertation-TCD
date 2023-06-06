0eaf5c01cb6344d6263799540d72c35782a34a16
ggn1 <ggn1@hw.ac.uk>
2023-06-06 18:45:00 +0400

    cleaned data

--- Code/utility_functions.py
+++ Code/utility_functions.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from colorama import Fore
 from colorama import Back
 from colorama import Style
@@ -33,24 +34,34 @@ def print_color(txt:str, fg:str, bg:str=None):
         Color Options: black, red, green, yellow, blue, 
                        magenta, cyan, white.
     '''
-    fg = fg.lower()
-    bg = bg.lower()
-
     # Color input sanity checks.
+    fg = fg.lower()
     if fg not in list(COLORS['fg'].keys()):
         raise Exception(
             "utility_functions>print_color()>'Invalid foreground color. "
             + f"Valid color options = {','.join(list(COLORS['fg'].keys()))}"
-            + f"{Style.RESET_ALL}"
         )
-    if bg not in list(COLORS['bg'].keys()):
-        raise Exception(
-            "utility_functions>print_color()>'Invalid background color. "
-            + f"Valid color options = {','.join(list(COLORS['bg'].keys()))}"
-            + f"{Style.RESET_ALL}"
-        )
+    if bg is not None:
+        bg = bg.lower() if bg is not None else bg
+        if bg not in list(COLORS['bg'].keys()):
+            raise Exception(
+                "utility_functions>print_color()>'Invalid background color. "
+                + f"Valid color options = {','.join(list(COLORS['bg'].keys()))}"
+            )
     
     # Print in color.
-    if bg is not None:
-        print(f'{COLORS["bg"][bg]}{COLORS["fg"][fg]}{txt}')
-    else: print(f'{COLORS["bg"][bg]}{txt}')
+    if bg is not None: print(f'{COLORS["bg"][bg]}{COLORS["fg"][fg]}{txt}{Style.RESET_ALL}')
+    else: print(f'{COLORS["fg"][fg]}{txt}{Style.RESET_ALL}')
+
+def desc_df(df:pd.core.frame.DataFrame):
+    ''' Describes a given pandas data frame. '''
+    print(f"No. of rows = {len(df)}\nNo. of columns = {len(df.columns)}")
+    print_color(f"\nFeature Overview", "yellow")
+    print(df.info())
+    print_color("\nValue Counts", "yellow")
+    for feature in df.columns:
+        vc = df[feature].value_counts()
+        print(f"\n{vc}")
+        print(f'No. of distinct values = {len(vc)}')
+        print(f'No. of missing values = {df[feature].isna().sum()}')
+    display(df.head())
